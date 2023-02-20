f9f9bfac75c947b5142272f32ab00078f1ab0196
SlothL0rd <46608252+SlothL0rd@users.noreply.github.com>
2023-02-19 23:37:38 -0500

    final commit

---
+++ Course.py
@@ -0,0 +1,9 @@
+class Course:
+    def __int__(self, name, number, section, term, year, size):
+        self.name = name
+        self.number = number
+        self.section = section
+        self.term = term
+        self.year = year
+        self.size = size
+
